10cc8bb0c828eea7099282a71ab1a537c3dc6d8f
Anjali <anjaliiiiiiii333@gmail.com>
2026-07-01 22:42:27 +0530

    add count of open ports,scan timmer and improve terminal output

--- scanner.py
+++ scanner.py
@@ -1,8 +1,10 @@
+
 import socket
+import time
 print("=" * 40)
 print("      PYTHON PORT SCANNER")
 print("=" * 40)
-target = input("Enter the IP address:")
+target = input("Target host:")
 services = {
     21: "FTP",
     22: "SSH",
@@ -10,12 +12,22 @@ services = {
     443: "HTTPS"
 }
 common_ports = [21, 22, 23, 25, 53, 80, 110, 143, 443]
+start_time=time.time()
+count = 0
 for port in common_ports:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)
     result=sock.connect_ex((target,port))
     if result == 0:
+      count+=1
       print(f"✅ Port {port} ({services.get(port,'Unknown')}) is OPEN")
     else:
         print(f"❌ Port {port} ({services.get(port,'Unknown')}) is CLOSED")
     sock.close()
+print("=" * 40)
+print("      SCAN COMPLETE")
+print(f"Total Open Ports: {count}")
+end_time=time.time()
+time_taken=end_time-start_time
+print(f"Time taken for scanning: {time_taken:.2f} seconds")
+print("=" * 40)
